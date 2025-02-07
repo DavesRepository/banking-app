0600d4fab59c0a5d6e867162da2ba8ed50df4015
Dave Weernink <Dave.weernink@gmail.com>
2025-02-07 16:40:58 +0100

    minor fixes

--- src/main/java/com/nextgen/bankingapp/services/AccountController.java
+++ src/main/java/com/nextgen/bankingapp/services/AccountController.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@AllArgsConstructor
 public class AccountController {
   @Autowired
   private CurrentAccountRepository currentAccountRepository;
